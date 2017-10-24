1387158c236a1fa291461e70dde60b47de2453a6
Dawid Łabno <dawidlabno@outlook.com>
2017-10-24 21:56:29 +0200

    Updating scores and changing the active user

--- app.js
+++ app.js
@@ -55,7 +55,24 @@ document.addEventListener("click", function roll(){
         
     }*/
 
-    document.querySelector('#current-' + activePlayer).textContent = dice;
+
+    if (dice !== 1) {
+        roundScore += dice;
+        //roundScore = roundScore + dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');   
+        document.querySelector('.player-1-panel').classList.toggle('active');     
+        
+        document.querySelector('.dice').style.display = 'none';
+    }
     
 });
 
